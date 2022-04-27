bec781263c9888ecb566a9dc6e57843c645d5242
atj <akselthyregod@live.dk>
2022-04-27 13:05:39 +0200

    soap javaee

---
+++ Warehouse/src/main/java/SoapClient.java
@@ -0,0 +1,19 @@
+import SOAPServices.IEmulatorService;
+
+public class SoapClient implements IEmulatorService {
+
+    @Override
+    public String pickItem(int trayId) {
+        return null;
+    }
+
+    @Override
+    public String insertItem(int trayId, String name) {
+        return null;
+    }
+
+    @Override
+    public String getInventory() {
+        return null;
+    }
+}
