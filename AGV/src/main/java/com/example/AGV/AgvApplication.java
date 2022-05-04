6e43006ca0c90e2d587a2a233d5753209428bc4f
Rasmusthye <rasmus@techdan.dk>
2022-05-04 10:17:25 +0200

    AGV put setup

---
+++ AGV/src/main/java/com/example/AGV/AgvApplication.java
@@ -0,0 +1,29 @@
+package com.example.AGV;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@SpringBootApplication
+public class AgvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgvApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ AGV/src/main/java/com/example/AGV/RestService.java
@@ -0,0 +1,20 @@
+package com.example.AGV;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestService {
+
+    private final RestTemplate restTemplate;
+
+    public RestService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public String getPostsPlainJSON() {
+        String url = "http://localhost:8082/v1/status";
+        return this.restTemplate.getForObject(url, String.class);
+    }
+}
